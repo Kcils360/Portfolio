86b98b2b5900b87fa42491b1d28a472b02ab0ceb
Gregory Dukes <kcils360@gmail.com>
2017-08-17 17:14:27 -0700

    got carousel functionality, need to tweek for my project

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,34 @@
 'use-strict';
 
 
-  $('li').on('click', function(){
-    var $aboutShow = $(this).data('content');
-    $('section').hide();
-    $('#' + $aboutShow).fadeIn(500);
-  });
-  // $('.main-nav .tab:first').click();
+// $('li').on('click', function() {
+//   var $aboutShow = $(this).data('content');
+//   $('section').hide();
+//   $('#' + $aboutShow).fadeIn(500);
+// });
+//
+//
+
+$(function() {
+  var width = 720;
+  var animationSpeed = 1000;
+  var pause = 3000;
+  var currentSlide = 1;
+
+  var $slider = $('#slider');
+  var $slideContainer = $slider.find('.slides');
+  var $slides = $slideContainer.find('.slide')
+
+  setInterval(function(){
+    $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
+      currentSlide++;
+      if(currentSlide === $slides.length){
+        currentSlide = 1;
+        $slideContainer.css('margin-left', 0);
+      }
+    });
+  }, pause);
+
+
+
+});

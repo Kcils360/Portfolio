cb6309628c58980c3aa60b34092838574d4fa717
Gregory Dukes <kcils360@gmail.com>
2017-08-17 20:42:08 -0700

    it's conditionally responsive.  it will set to browser width on page reload

--- scripts/app.js
+++ scripts/app.js
@@ -10,17 +10,24 @@
 //
 
 $(function() {
-  var width = 720;
+  var width = $(window).width();
+  var height = $(window).height();
+  // var width = 1920;
+
   var animationSpeed = 1000;
   var pause = 3000;
   var currentSlide = 1;
 
+  var $image = $('img');
   var $slider = $('#slider');
   var $slideContainer = $slider.find('.slides');
   var $slides = $slideContainer.find('.slide')
-
   setInterval(function(){
     $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
+      $slideContainer.css('height', height);
+      $slider.css('max-width', width);
+      $slides.css('max-width', width);
+      $image.css('width', width);
       currentSlide++;
       if(currentSlide === $slides.length){
         currentSlide = 1;

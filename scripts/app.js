6519092d52ebb865e14c443fa079ba80f425f07b
Gregory Dukes <kcils360@gmail.com>
2017-08-18 10:48:15 -0700

    cleaned up code, got a backgroud color on my menu

--- scripts/app.js
+++ scripts/app.js
@@ -9,33 +9,29 @@
 //
 //
 
+//--------------------------carousel------------------------------
 $(function() {
   var width = $(window).width();
   var height = $(window).height();
-  // var width = 1920;
-
-  var animationSpeed = 1000;
-  var pause = 3000;
+  // var animationSpeed = 1000;
+  // var pause = 3000;
   var currentSlide = 1;
-
   var $image = $('img');
   var $slider = $('#slider');
   var $slideContainer = $slider.find('.slides');
   var $slides = $slideContainer.find('.slide')
+  $slideContainer.css('height', height);
+  $slider.css('max-width', width);
+  $slides.css('max-width', width);
+  $image.css('width', width);
   setInterval(function(){
-    $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
-      $slideContainer.css('height', height);
-      $slider.css('max-width', width);
-      $slides.css('max-width', width);
-      $image.css('width', width);
+    $slideContainer.animate({'margin-left': '-='+width}, 1000, function(){
       currentSlide++;
       if(currentSlide === $slides.length){
         currentSlide = 1;
         $slideContainer.css('margin-left', 0);
       }
     });
-  }, pause);
-
-
-
+  }, 3000);
 });
+//----------------------------------------------------------------

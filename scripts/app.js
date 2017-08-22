8a516f6e369ea42ac9a3d3cfef4577a1ca67c279
Gregory Dukes <kcils360@gmail.com>
2017-08-22 15:53:35 -0700

    finished SMACSS refactoring

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,35 @@
 'use-strict';
 
-var handleMainNav = function(){
+(function handleMainNav(){
   $('.tab-content').hide();
   var $menuList = $('.menu li');
   $($menuList).on('click', function() {
     console.log(this.className);
     $('#slider').hide();
     $('#tab').hide();
-    $('#' + this.className).fadeIn(500);
+    $('#' + this.className).fadeIn(700);
+    $('.menu').slideToggle();
   });
 
-
-$('#hamburger').click(function(){
-  $('.menu').slideToggle();
-});
-}
-handleMainNav();
+  $('#hamburger').click(function(){
+    $('.menu').slideToggle();
+  });
+})()
 
 //--------------------------carousel------------------------------
 $(function() {
   var width = $(window).width();
   var height = $(window).height();
-  // var animationSpeed = 1000;
-  // var pause = 3000;
+
   var currentSlide = 1;
-  var $image = $('img');
   var $slider = $('#slider');
   var $slideContainer = $slider.find('.slides');
-  var $slides = $slideContainer.find('.slide')
+  var $slides = $slideContainer.find('.slide');
+
   $slideContainer.css('height', height);
   $slider.css('max-width', width);
   $slides.css('max-width', width);
-  $image.css('width', width);
+  $('img').css('width', width);
   setInterval(function(){
     $slideContainer.animate({'margin-left': '-='+width}, 1500, function(){
       currentSlide++;

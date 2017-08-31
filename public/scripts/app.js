fc6c6b301244d265fa72d3301d7c7eb984085e74
Gregory Dukes <kcils360@gmail.com>
2017-08-31 09:04:55 -0700

    commenting out app.js to get img on DOM

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,60 +8,60 @@ const hideContent = function(){
   $('#about').hide();
   $('#project').hide();
 };
-
-hideContent();
-if($width<$height){
-  $('#sliderTall').show();
-} else{
-  $('#slider').show();
-}
-(function handleMainNav(){
-  const $menuList = $('.menu li');
-  $($menuList).on('click', function() {
-    console.log(this.className);
-    hideContent();
-    $('#' + this.className).fadeIn(700);
-    if($width < $height){
-      $('.menu').slideToggle();
-    }
-  });
-
-  $('#hamburger').click(function(){
-    $('.menu').slideToggle();
-  });
-})()
-
-//--------------------------carousel------------------------------
-$(function() {
-
-  let currentSlide = 1;
-  let $slider = $('#slider');
-  let $slideContainer = $slider.find('.slides');
-  let $slides = $slideContainer.find('.slide');
-  let $slidesImg = $slides.find('img')
-
-  if($width < $height){
-    $slider = $('#sliderTall');
-    $slideContainer = $slider.find('.slidesTall');
-    $slides = $slideContainer.find('.slideTall');
-    $slidesImg = $slides.find('img')
-  }
-
-  $slideContainer.css('height', $height);
-  $slider.css('max-width', $width);
-  $slides.css('max-width', $width);
-  $slidesImg.css('width', $width);
-  setInterval(function(){
-    $slideContainer.animate({'margin-left': '-='+$width}, 1500, function(){
-      currentSlide++;
-      if(currentSlide === $slides.length){
-        currentSlide = 1;
-        $slideContainer.css('margin-left', 0);
-      }
-    });
-  }, 5000);
-});
-//----------------------------------------------------------------
+// 
+// hideContent();
+// if($width<$height){
+//   $('#sliderTall').show();
+// } else{
+//   $('#slider').show();
+// }
+// (function handleMainNav(){
+//   const $menuList = $('.menu li');
+//   $($menuList).on('click', function() {
+//     console.log(this.className);
+//     hideContent();
+//     $('#' + this.className).fadeIn(700);
+//     if($width < $height){
+//       $('.menu').slideToggle();
+//     }
+//   });
+//
+//   $('#hamburger').click(function(){
+//     $('.menu').slideToggle();
+//   });
+// })()
+//
+// //--------------------------carousel------------------------------
+// $(function() {
+//
+//   let currentSlide = 1;
+//   let $slider = $('#slider');
+//   let $slideContainer = $slider.find('.slides');
+//   let $slides = $slideContainer.find('.slide');
+//   let $slidesImg = $slides.find('img')
+//
+//   if($width < $height){
+//     $slider = $('#sliderTall');
+//     $slideContainer = $slider.find('.slidesTall');
+//     $slides = $slideContainer.find('.slideTall');
+//     $slidesImg = $slides.find('img')
+//   }
+//
+//   $slideContainer.css('height', $height);
+//   $slider.css('max-width', $width);
+//   $slides.css('max-width', $width);
+//   $slidesImg.css('width', $width);
+//   setInterval(function(){
+//     $slideContainer.animate({'margin-left': '-='+$width}, 1500, function(){
+//       currentSlide++;
+//       if(currentSlide === $slides.length){
+//         currentSlide = 1;
+//         $slideContainer.css('margin-left', 0);
+//       }
+//     });
+//   }, 5000);
+// });
+// //----------------------------------------------------------------
 
 $.get('/github/user/repos')
 .then(data => data.forEach(repo =>
